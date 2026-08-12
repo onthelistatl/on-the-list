a6b8c7701d1763fb5d31c457afab73b02b8609d5
On The List <onthelistteam@gmail.com>
2026-08-11 21:10:58 -0400

    Add Virginia Highland venues migration

---
+++ supabase/07_virginia_highland.sql
@@ -0,0 +1,9 @@
+insert into clubs (name, neighborhood, city, genre, dress_code, last_call, cover_tiers) values
+  ('Dark Horse Tavern', 'Virginia Highland', 'Atlanta', 'Bar / Live Music', 'Casual', '3:00 AM',
+    '[{"until": null, "price": 0}]'::jsonb),
+  ('Moe''s & Joe''s', 'Virginia Highland', 'Atlanta', 'Dive Bar / Tavern', 'Casual', '2:00 AM',
+    '[{"until": null, "price": 0}]'::jsonb),
+  ('Neighbor''s Pub', 'Virginia Highland', 'Atlanta', 'Pub / Sports Bar', 'Casual', '3:00 AM',
+    '[{"until": null, "price": 0}]'::jsonb),
+  ('Dad''s', 'Virginia Highland', 'Atlanta', 'Cocktail Bar', 'Casual', '2:00 AM',
+    '[{"until": null, "price": 0}]'::jsonb);
